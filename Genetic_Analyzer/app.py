9d818d59e370a9e695d7d68fa93705a61c50c066
LexInn006 <marcoalexius09@gmail.com>
2026-06-11 02:29:26 +0700

    biar bisa dideploy

--- Genetic_Analyzer/app.py
+++ Genetic_Analyzer/app.py
@@ -128,5 +128,6 @@ def health():
     return jsonify({"status": "ok", "akurasi_model": meta["accuracy"]})
 
 if __name__ == "__main__":
-    print("Backend berjalan di http://localhost:5000")
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
